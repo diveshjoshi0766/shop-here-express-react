9cd30c71dab60c33065b264f2c4e619642cd2260
diveshjoshi0766 <diveshjoshi0766@gmail.com>
2021-07-12 23:48:10 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,17 @@ app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
     res.send('Server is ready');
   });
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
 });
-
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
